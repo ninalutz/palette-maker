7750def27e605b562126e1b5381120397bdb3db2
Nina Lutz <nlutz@mit.edu>
2020-01-06 16:54:09 -0500

    Adding some functions and finding JS for image

--- javascript/kmeansHSV-runner.js
+++ javascript/kmeansHSV-runner.js
@@ -51,10 +51,11 @@ class KMeansHSVRunner {
   computeMeans(groups) {
     return _.map(groups, (group) => {
       let averageColor = ImageUtil.computeAverageColor(group);
+      let hsv = rgbToHsl(averageColor.red, averageColor.green, averageColor.blue);
       return {
-        red: averageColor.green,
-        green: averageColor.green,
-        blue: averageColor.green
+        red: hsv[0],
+        green: hsv[1],
+        blue: hsv[2]
       };
     });
   }
@@ -105,6 +106,29 @@ class KMeansHSVRunner {
       return diff * diff;
     });
     return Math.sqrt(_.sum(squaredDiffs));
+  }
+
+  rgbToHsl(r, g, b) {
+      r /= 255, g /= 255, b /= 255;
+
+      var max = Math.max(r, g, b), min = Math.min(r, g, b);
+      var h, s, l = (max + min) / 2;
+
+      if (max == min) {
+        h = s = 0; // achromatic
+      } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+        switch (max) {
+          case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+          case g: h = (b - r) / d + 2; break;
+          case b: h = (r - g) / d + 4; break;
+        }
+
+        h /= 6;
+    }
+    return [ h, s, l ];
   }
 
 }
